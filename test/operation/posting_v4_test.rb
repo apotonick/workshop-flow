20d5b2b3b9210324657cc6a1509ec15c1184e95b
Nick Sutterer <apotonick@gmail.com>
2023-07-16 13:37:43 +0200

    we can use MyAI.

---
+++ test/operation/posting_v4_test.rb
@@ -0,0 +1,56 @@
+require "test_helper"
+# Goal: we can use any callable as a binary step.
+
+class OperationPostingTest_v3 < Minitest::Spec
+  class MyAI
+    class Formatter
+      def self.call(ctx, params:, **)
+        params[:content] = params[:content].chomp # it's magic! :sparkles:
+        # Hm... mutating params is not a good idea, though.
+      end
+    end
+  end
+
+  module Posting::Operation
+    class Create < Trailblazer::Operation
+      step :validate
+      left :validate_error
+      step MyAI::Formatter # AI formatting
+      step :create_slug
+      step :save
+      step :notify
+
+      def validate(ctx, params:, **)
+        return unless params.key?(:content)
+        params[:content].size >= 10
+      end
+
+      # def reformat(ctx, params:, **)
+      #   true
+      # end
+
+      def create_slug(ctx, **)
+        true
+      end
+
+      def save(ctx, params:, **)
+        ctx[:model] = Posting.new
+      end
+
+      def notify(ctx, model:, **)
+        model.present?
+      end
+
+      def validate_error(ctx, **)
+        ctx[:errors] = {content: ["There have been errors."]}
+      end
+    end
+  end
+
+  it "works" do
+    result = Posting::Operation::Create.wtf?(params: {content: "Have you ever tried a fresh Gin Dobre?\nIt is a beautif\n"})
+
+    assert_equal result.success?, true
+    assert_equal result[:params], {content: "Have you ever tried a fresh Gin Dobre?\nIt is a beautif"}
+  end
+end

c8c7699857f022559500620d621245ce7fbd6c29
Nick Sutterer <apotonick@gmail.com>
2023-07-16 13:17:50 +0200

    01 test running.

--- test/operation/posting_test.rb
+++ test/operation/posting_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
-class OperationPostingTest < Minitest::Spec
+# Goal: have as many beers tonight as possible.
+
+class OperationPostingTest_v1 < Minitest::Spec
   module Posting::Operation
     class Create < Trailblazer::Operation
+      # this could also be written as:
+      # TODO
+
       step :validate
       step :reformat # AI formatting
       step :create_slug
@@ -13,27 +18,27 @@ class OperationPostingTest < Minitest::Spec
         params.any?
       end
 
-      def reformat(ctx, validated_input:, **)
-
+      def reformat(ctx, params:, **)
+        true
       end
 
       def create_slug(ctx, **)
-
+        true
       end
 
-      def save(ctx, validated_input:, **)
-
+      def save(ctx, params:, **)
+        ctx[:model] = Posting.new
       end
 
       def notify(ctx, model:, **)
-
+        model.present?
       end
     end
   end
 
-  it "works" do
+  it "fails, because we need to learn TRB" do
     result = Posting::Operation::Create.wtf?(params: {})
 
-    assert_equal result.success?, true
+    assert_equal result.failure?, true
   end
 end
